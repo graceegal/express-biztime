95e54a4efd2ea67379ede62d41fb953abc98735c
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-03-21 12:35:31 -0700

    created all company routes

--- routes/companies.js
+++ routes/companies.js
@@ -34,10 +34,8 @@ router.get("/:code", async function (req, res, next) {
          FROM companies
          WHERE code = $1`, [code]);
 
-
-
   const company = results.rows;
-  
+
   if (company.length === 0) {
     throw new NotFoundError("Company code does not exist.");
   }
@@ -45,6 +43,75 @@ router.get("/:code", async function (req, res, next) {
 });
 
 
+
+
+/** Create new company, returning {company: {code, name, description}};
+ * Accepts json body: {code, name, description}
+*/
+
+router.post("/", async function (req, res, next) {
+  if (!req.body) throw new BadRequestError('Missing company information.');
+
+  const { code, name, description } = req.body;
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+           VALUES ($1, $2, $3)
+           RETURNING code, name, description`,
+    [code, name, description],
+  );
+  const company = result.rows[0];
+  return res.status(201).json({ company });
+});
+
+
+
+/** Update company, returning {company: {code, name, description}};
+ * Accepts json body: {name, description}
+*/
+
+router.put("/:code", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+  const { name, description } = req.body;
+
+  const result = await db.query(
+    `UPDATE companies
+           SET name=$1,
+           description=$2
+           WHERE code = $3
+           RETURNING code, name, description`,
+    [name, description, req.params.code],
+  );
+  const company = result.rows[0];
+
+  if (!company) {
+    throw new NotFoundError("Company does not exist.");
+  }
+  return res.json({ company });
+});
+
+
+/** Delete company, returning {status: "deleted"};
+ * Takes in company code as URL parameter
+ */
+
+router.delete("/:code", async function (req, res, next) {
+  const results = await db.query(
+    `SELECT code, name, description
+          FROM companies
+          WHERE code = $1`, [request.params.code]);
+
+  const company = results.rows;
+  if (company.length === 0) {
+    throw new NotFoundError("Company code does not exist.");
+  }
+
+  const result = await db.query(
+    "DELETE FROM companies WHERE code = $1",
+    [req.params.code],
+  );
+
+  return res.json({ status: "deleted" });
+});
 
 
 module.exports = router;
